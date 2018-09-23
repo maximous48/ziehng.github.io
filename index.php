11dbb2d07dde791735853d24c901177df05ca95a
maximous48 <43507652+maximous48@users.noreply.github.com>
2018-09-23 21:53:19 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Got Hacked ?...</title>
+<script>
+alert("this is ./MaXimous48");
+alert("Devil Security Crew");
+</script>
+<meta charset="UTF-8">
+<meta name="Author" content="./MaXimous48"/<meta name="copyright" content="./MaXimous48"/>
+<meta name="description" content="./MaXimous48 into your system"/>
+<link rel="icon" type="image/gif" href="http://imageupload.co.uk/images/2018/09/15/images10.md.jpg">
+<link href='http://fonts.googleapis.com/css?family=Ruslan+Display' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Covered+By+Your+Grace' rel='stylesheet' type='text/css'>
+<style type="text/css">
+			body {
+				overflow:hidden;
+				background-image:url(http://img.jetbitts.com/screensavers/down/cartoon-anime/nyancat_pkxh82km.gif);
+				background-color: #000000;
+				background-size: cover;
+				margin: 0px;
+				background-position:0% 0%;
+				background-attachment:fixed;
+				background-repeat:no-repeat;
+			}
+</style>
+<br>
+<br>
+<center><b>
+<div style="text-shadow: 5px 0px 5px red;">
+<font face="Ruslan Display" size="30" color="silver">HACKED ? ./MaXimous48<br /></div></b>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br><i>
+<div style="text-shadow: 5px 0px 5px blue;">
+<font face="Love Ya Like A Sister" size="6" color="silver">Nothing Security In Your System or I find Bug on your website and it is not secure</i>
+<br>
+<br>
+<font face="Covered By Your Grace" size="5" color="red">==> GreeTz <==</font>
+<br />
+<font face="Love Ya Like a Sister" size="3" color="silver">SecDevil13 - ./ScrmDevil_ - ./CE19 - MR.K4L1L1NUX#00 - ./Lonely - Countrix - 4LM05TH3V!L - @TS14 - All Defacer Indonesia + And My Team Devil Security Crew
